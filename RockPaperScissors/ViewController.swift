5091c81e6ebade4de3dc0c6a95ab2d690098297f
Samir Marin <marin.samir@gmail.com>
2015-12-01 00:07:29 -0800

    Initial Commit

---
+++ RockPaperScissors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RockPaperScissors
+//
+//  Created by Samir Marin on 2015-12-01.
+//  Copyright © 2015 Samir Marin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
