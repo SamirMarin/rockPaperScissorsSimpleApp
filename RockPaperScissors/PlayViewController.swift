d16aa4329b96145b0dd4ad5600c58673c38a183f
Samir Marin <marin.samir@gmail.com>
2015-12-02 01:19:45 -0800

    game mostly done.. added all game function scissor, paper, rock, tie -- play against computer-- still need back/cancel button once have played in order to be able to play again

--- RockPaperScissors/PlayViewController.swift
+++ RockPaperScissors/PlayViewController.swift
@@ -14,6 +14,7 @@ class PlayViewController: UIViewController {
     @IBOutlet weak var paper: UIButton!
     @IBOutlet weak var scissors: UIButton!
     var userChoice: Int?
+    var paperRockScissor: Int?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -31,26 +32,70 @@ class PlayViewController: UIViewController {
     // 2 user
     private func pickWinnerAndType()->String{
         if(generateRandomPick() == 1 && userChoice == 3){
+            paperRockScissor = 1
             return "rock destroys scissors! you lose!"
         }
         else if(generateRandomPick() == 1 && userChoice == 2){
+            paperRockScissor = 2
             return "paper covers rock! you win!"
         }
         else if(generateRandomPick() == 2 && userChoice == 1){
+            paperRockScissor = 2
             return "paper covers rock! you lose!"
         }
-        else{
+        else if(generateRandomPick() == 2 && userChoice == 3){
+            paperRockScissor = 3
+            return "scissor cuts paper! you win!"
+        }
+        else if(generateRandomPick() == 3 && userChoice == 1){
+            paperRockScissor = 1
             return "rock destroys scissors! you win!"
         }
+        else if(generateRandomPick() == 3 && userChoice == 2){
+            paperRockScissor = 3
+            return "scissor cuts paper! you lose!"
+        }
+        else{
+            return "it's a TIE!!!"
+        }
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "paperPick"{
+            let controller = segue.destinationViewController as! RockPaperScissorViewController
+            setControllerVar(controller)
+            
+    }
+        else if segue.identifier == "scissorPick"{
+            let controller = segue.destinationViewController as! RockPaperScissorViewController
+            setControllerVar(controller)
+            
+            
+        }
+    }
+    
+    private func setControllerVar(controller: RockPaperScissorViewController){
+        controller.result = pickWinnerAndType()
+        controller.rockPaperScissor = self.paperRockScissor
+        
+    }
+    
 
     @IBAction func rockPick(sender: UIButton) {
         userChoice = 1
+        let controller: RockPaperScissorViewController
+        
+        controller = self.storyboard?.instantiateViewControllerWithIdentifier("RockPaperScissorViewController") as! RockPaperScissorViewController
+        setControllerVar(controller)
+        
+        self.presentViewController(controller, animated: true, completion: nil)
+        
         
     }
     
     @IBAction func paperPick(sender: UIButton) {
         userChoice = 2
+        performSegueWithIdentifier("paperPick", sender: self)
     }
     
 
--- RockPaperScissors/RockPaperScissorsViewController.swift
+++ RockPaperScissors/RockPaperScissorsViewController.swift
@@ -10,7 +10,36 @@ import UIKit
 
 class RockPaperScissorViewController: UIViewController{
     
+    @IBOutlet weak var resultImage: UIImageView!
+    @IBOutlet weak var display: UILabel!
     
+    
+    var result: String?
+    var rockPaperScissor: Int?
+    
+    override func viewWillAppear(animated: Bool) {
+        if let labelResult = self.result{
+            display.text = labelResult
+        }
+        else{
+            display.text = "Something actually went wrong it's not actually a tie :("
+        }
+        if let choice = self.rockPaperScissor{
+            if(choice == 1){
+                resultImage.image = UIImage(named: "PaperWinsToRock")
+                
+            }
+            else if(choice == 2){
+                resultImage.image = UIImage(named: "RockWinsToScissors")
+            }
+            else if(choice == 3){
+                resultImage.image = UIImage(named: "ScissorWinsToPaper")
+            }
+        }
+        else{
+            resultImage.image = UIImage(named: "Tie")
+        }
+    }
 }
 
 
